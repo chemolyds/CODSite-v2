ac79a69c9815729a08382c48a1da9b9c1589a2dd
Jerdan1980 <jeremytinana@gmail.com>
2021-06-01 12:49:38 -0400

    Make Layouts prettier

---
+++ src/layouts/FAQ.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql, Link } from 'gatsby'
+
+import Layout from './layout.js'
+
+const FAQ = ({ children, pageContext }) => (
+	<Layout>
+		<div class="grid grid-cols-2">
+			<div class="relative">
+				<h1>{pageContext.frontmatter.title}</h1>
+			</div>
+			<div class="relative text-right text-purple-700">
+				<h1><Link to="/FAQ" class="">Back</Link></h1>
+			</div>
+		</div>
+		<div>
+			{children}
+		</div>
+	</Layout>
+)
+
+FAQ.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default FAQ
--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import { graphql, Link } from "gatsby"
-import Layout from '../components/layout'
+import Layout from '../layouts/layout'
 
 export default function FAQ({ data }) {
 	return (
 		<Layout>
-			<h1>Frequently Asked Questions</h1>
-			<ul>
-			{
-				data.allFile.nodes.filter(node => node.childMdx).map(node => {
-					return (
-						<li><Link to={node.childMdx.slug}>{node.childMdx.slug}</Link></li>
-					)
-				})
-			}
-			</ul>
+			<h1 class="lg:text-center">Frequently Asked Questions</h1>
+			<dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+				{
+					data.allFile.nodes.filter(node => node.childMdx).map(node => { return (
+						<div class="relative">
+							<Link to={node.childMdx.slug}>
+							<dt>
+								<p class="mb-2 text-lg leading-5 front-medium text-gray-900">
+									{node.childMdx.frontmatter.title}
+								</p>
+							</dt>
+							<dd class="text-base text-gray-500">
+								{node.childMdx.excerpt}
+							</dd>
+							</Link>
+						</div>
+					)})
+				}
+			</dl>
 			
 		</Layout>
 	)
@@ -27,6 +36,9 @@ query {
 			childMdx {
 				slug
 				excerpt
+				frontmatter {
+					title
+				}
 			}
 		}
 	}
