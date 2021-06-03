12ab504b325d43cf1978ad8adfad540449ca2507
Jerdan1980 <jeremytinana@gmail.com>
2021-06-02 23:00:21 -0400

    Change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,13 +78,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-default-mdx-basic',
-        short_name: 'starter',
+        name: 'Chemistry Olympiads Discord Server Website',
+        short_name: 'CODSite',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/cods_logo_gradient.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
