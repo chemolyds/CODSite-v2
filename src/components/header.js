beca05900a67266d9f2f2f24676c97154904879e
Tarun Agarwal <tarun.agarwal2004@gmail.com>
2021-06-04 19:22:41 +0530

    Revamped navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,14 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React, {useState} from 'react'
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
-
-const Menu = {display: 'inline',
-  color: 'white',
-  textAlign: 'center',
-  padding: '14px 16px',
-  textDecoration: 'none',
-  float: 'right',
-  position:'relative'
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-const dropdown = {
-  boxShadow: "2px 2px 10px #ccc",
-  textAlign: 'left',
-  padding: "14px",
-  color:"black",
-  float: 'right', 
-  position:"absolute",
-  background:"white",
-  border:"0px solid red",
-  borderRadius:"12px",
-  display: "flex",
-  flexDirection: "column",
-  overflow: "auto",
-  top:"40px",
-  maxWidth:"200px",
-  maxHeight: "120px"
-}
 
 
 
@@ -40,87 +20,113 @@ const Header = function({ siteTitle }) {
   const [isShownAbout, setIsShownAbout] = useState(false);
 
   return (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div 
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 id="test-id" style={{display: 'inline', margin: 0, marginRight: 10 }}>
+  <div class="antialiased bg-gray-700 font-sans text-white mb-20 sticky top-0">
+    <div class="lg:px-16 px-6 flex flex-wrap items-center lg:py-0 py-2 mr-auto">
+      <h1 id="test-id" class="m-0 hover:text-blue-200 font-light tracking-wider">
         <Link
           to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+         >
           {siteTitle}
         </Link>
       </h1>
 
-			<ul style={{display: 'inline', listStyleType: 'none', margin: 0, padding: 0}}>
+			<ul class="my-0 ml-auto">
 				{/* It goes right to left */}
-        {/*there are menus commented out for each of the links if we need to add them in in the future*/}
+       
 				<li style={{display: 'inline'}}>
-          <div style={Menu} onMouseLeave={() => setIsShownAbout(false)}>
-            {/*isShownAbout && (
-              <div style={dropdown}>
-                <p>Put Links here</p>
-                <p>Link 2</p>
-                <p>Link 3</p>
-              </div>)
-            */}
+          <div class="hover:text-gray-300 m-0 py-2 px-4 relative float-right" onMouseLeave={() => setIsShownAbout(false)}>
             <Link to="/about" onMouseEnter={() => setIsShownAbout(true)}>About</Link>
           </div>    
         </li>
 
 				<li style={{display: 'inline'}}>
-          <div style={Menu} onMouseLeave={() => setIsShownFAQ(false)}>
-            {/*isShownFAQ && (
-              <div style={dropdown}>
-                <p>Put Links here</p>
-                <p>Link 2</p>
-                <p>Link 3</p>
-              </div>)
-            */}
-            <Link to="/FAQ" onMouseEnter={() => setIsShownFAQ(true)}>FAQ</Link>
+          <div class="hover:text-gray-300 m-0 py-2 px-4 relative float-right" onMouseLeave={() => setIsShownFAQ(false)}>
+           <Link to="/FAQ" onMouseEnter={() => setIsShownFAQ(true)}>FAQ</Link>
           </div>
         </li>
 
 				<li style={{display: 'inline'}}>
-          <div style={Menu} onMouseLeave={() => setIsShownGuides(false)}>
-            {/*isShownGuides && (
-              <div style={dropdown}>
-                <p>Put Links here</p>
-                <p>Link 2</p>
-                <p>Link 3</p>
-              </div>)
-            */}
+          <div class="hover:text-gray-300 m-0 py-2 px-4 relative float-right" onMouseLeave={() => setIsShownGuides(false)}>
             <Link to="/guides" onMouseEnter={() => setIsShownGuides(true)}>Guides</Link>
           </div>
         </li>
-				<li style={{display: 'inline'}}>
-          <div style={Menu} onMouseLeave={() => setIsShownCourses(false)}>
-            {isShownCourses && (
-              <div style={dropdown}>
-                <Link style={{margin:"3px"}} to="/courses/Bronze">Bronze</Link>
-                <Link style={{margin:"3px"}} to="/courses/Silver">Silver</Link>
-                <Link style={{margin:"3px"}} to="/courses/Gold">Gold</Link>
-              </div>)
-            }
-            <Link to="/courses" onMouseEnter={() => setIsShownCourses(true)}>Courses</Link>
+
+        {/* Dropdown for courses */}
+				<Menu as="div" className="relative inline-block text-left ">
+      {({ open }) => (
+        <>
+          <div>
+            <Menu.Button className="inline-flex justify-center w-full rounded-md px-4 py-2  text-sm font-medium text-white focus:outline-none hover:text-gray-300">
+              Courses
+              <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+            </Menu.Button>
           </div>
-        </li>
+
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              static
+              className="origin-top-right absolute right-0 mt-2 w-28 rounded-md shadow-xl bg-gray-700 ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
+            >
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="\courses\Bronze"
+                      className={classNames(
+                        active ? 'bg-gray-600 text-gray-100' : 'text-white',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Bronze
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="\courses\Silver"
+                      className={classNames(
+                        active ? 'bg-gray-600 text-gray-100' : 'text-white',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Silver
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="\courses\Gold"
+                      className={classNames(
+                        active ? 'bg-gray-600 text-gray-100' : 'text-white',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Gold
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+              
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
 			</ul>
     </div>
   </div>
+  
   )
 }
 
