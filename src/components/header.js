a28fc58c17c793b54cf380a9d1a78d270aeeb9f6
Tarun Agarwal <tarun.agarwal2004@gmail.com>
2021-06-05 00:20:11 +0530

    Competitions added in navbar

--- src/components/header.js
+++ src/components/header.js
@@ -122,6 +122,92 @@ const Header = function({ siteTitle }) {
         </>
       )}
     </Menu>
+
+    {/* Dropdown for competitions */}
+    <Menu as="div" className="relative inline-block text-left ">
+      {({ open }) => (
+        <>
+          <div>
+            <Menu.Button className="inline-flex justify-center w-full rounded-md px-4 py-2 m-0 text-white focus:outline-none hover:text-gray-300">
+              Competitions
+              <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+            </Menu.Button>
+          </div>
+
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              static
+              className="origin-top-right absolute right-0 mt-2 w-28 rounded shadow-xl bg-gray-700 ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
+            >
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/about/competitions/acot"
+                      className={classNames(
+                        active ? 'bg-gray-600 text-gray-100' : 'text-white',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      ACOT
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/about/competitions/scho"
+                      className={classNames(
+                        active ? 'bg-gray-600 text-gray-100' : 'text-white',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      SChO
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/about/competitions/stc"
+                      className={classNames(
+                        active ? 'bg-gray-600 text-gray-100' : 'text-white',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      STC
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/about/competitions/wcc"
+                      className={classNames(
+                        active ? 'bg-gray-600 text-gray-100' : 'text-white',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      WCC
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+              
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
 			</ul>
     </div>
   </div>
