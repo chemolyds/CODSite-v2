c6febb6e7ed1ff83317b7398d4100ff50c9f8fb4
Jerdan1980 <jeremytinana@gmail.com>
2021-06-16 17:25:49 -0400

    Add course grouping

--- src/pages/guides.js
+++ src/pages/guides.js
@@ -3,19 +3,56 @@ import { graphql, Link } from "gatsby"
 import Layout from '../layouts/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 
+// https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects?page=1&tab=votes#tab-top
+const groupBy = function (xs, key) {
+	return xs.reduce(function (rv, x) {
+		let v = key instanceof Function ? key(x) : x[key];
+		let el = rv.find((r) => r && r.key === v);
+		if (el) {
+			el.values.push(x);
+		} else {
+			rv.push({ key: v, values: [x] });
+		}
+		return rv;
+	}, []);
+}
+
 export default function guides({ data }) {
 	return (
 		<Layout>
 			<StaticImage src="../images/guides.png" placeholder="blurred"/>
-			<ul>
 			{
-				data.allFile.nodes.filter(node => node.childMdx).map(node => {
+				groupBy(data.allFile.nodes, "relativeDirectory").map(group => {
 					return (
-						<li><Link to={node.childMdx.slug}>{node.childMdx.slug}</Link></li>
+						<div>
+							<h1>{group.key ? group.key.replace('_', ' ') : "misc"}</h1>
+							<table class="min-w-full divide-y divide-gray-200">
+								<thead>
+									<tr>
+										<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+										<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>
+									</tr>
+								</thead>
+								<tbody>
+									{
+										group.values.map(node => {
+											var slug = node.childMdx.slug
+											var title = node.childMdx.frontmatter.title
+											var description = node.childMdx.frontmatter.description
+											return (
+												<tr key={slug}>
+													<td class="px-6 py-4 whitespace-nowrap"><Link to={slug}>{title}</Link></td>
+													<td class="px-6 py-4 whitespace-nowrap"><Link to={slug}>{description}</Link></td>
+												</tr>
+											)
+										})
+									}
+								</tbody>
+							</table>
+						</div>
 					)
 				})
 			}
-			</ul>
 			
 		</Layout>
 	)
@@ -27,8 +64,12 @@ query {
 		nodes {
 			childMdx {
 				slug
-				excerpt
+				frontmatter {
+					title
+					description
+				}
 			}
+			relativeDirectory
 		}
 	}
 }
