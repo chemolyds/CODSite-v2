cacd28c7f18f828a34bd2c9bcffc2ce52ab53866
Jerdan1980 <jeremytinana@gmail.com>
2021-07-16 13:55:53 -0400

    Use fancy squares for courses

---
+++ src/pages/courses.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../layouts/layout'
+
+export default function courses() {
+	return (
+		<Layout>
+			<h1>Courses</h1> 
+			<dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+					<div class="max-w-md py-4 px-5 bg-white hover:bg-blue-100 shadow-2xl rounded-md mx-5">
+						<Link to="Bronze">
+						<dt>
+							<p class="mb-2 text-lg leading-5 front-medium text-gray-900">
+								Bronze
+							</p>
+						</dt>
+						<dd class="text-base text-gray-500">
+							Start here!
+						</dd>
+						</Link>
+					</div>
+					<div class="max-w-md py-4 px-5 bg-white hover:bg-blue-100 shadow-2xl rounded-md mx-5">
+						<Link to="Silver">
+						<dt>
+							<p class="mb-2 text-lg leading-5 front-medium text-gray-900">
+								Silver
+							</p>
+						</dt>
+						<dd class="text-base text-gray-500">
+							Nationals difficulty competitions
+						</dd>
+						</Link>
+					</div>
+					<div class="max-w-md py-4 px-5 bg-white hover:bg-blue-100 shadow-2xl rounded-md mx-5">
+						<Link to="Gold">
+						<dt>
+							<p class="mb-2 text-lg leading-5 front-medium text-gray-900">
+								Gold
+							</p>
+						</dt>
+						<dd class="text-base text-gray-500">
+							Mendelev difficulty competitions
+						</dd>
+						</Link>
+					</div>
+			</dl>
+		</Layout>
+	)
+}
