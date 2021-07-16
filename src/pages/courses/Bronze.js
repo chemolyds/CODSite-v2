492261decd34b648e12a1764464631ab0855f62f
Jerdan1980 <jeremytinana@gmail.com>
2021-07-16 15:08:07 -0400

    Clean up table code

--- src/pages/courses/Bronze.js
+++ src/pages/courses/Bronze.js
@@ -1,25 +1,14 @@
 import React from 'react'
 import { graphql, Link } from "gatsby"
 import Layout from '../../layouts/layout'
+import SubpagesTable from '../../components/SubpagesTable'
 
 export default function Bronze({ data }) {
 	return (
 		<Layout>
 			<h1>Bronze</h1>
 			<Link to='/courses'>Back to Courses</Link>
-			<ul>
-			{
-				data.allFile.nodes.filter(node => node.childMdx).filter(node => node.childMdx.excerpt).map(node => {
-					var slug = node.childMdx.slug
-					var title = node.childMdx.frontmatter.title
-					return (
-						<li><Link to={slug}>
-							{title ? title : slug}
-						</Link></li>
-					)
-				})
-			}
-			</ul>
+			<SubpagesTable nodes={data.allFile.nodes}/>
 			
 		</Layout>
 	)
@@ -27,7 +16,7 @@ export default function Bronze({ data }) {
 
 export const query = graphql`
 query {
-	allFile(filter: {sourceInstanceName: {eq: "Bronze"}}) {
+	allFile(filter: {sourceInstanceName: {eq: "Bronze"}, childMdx: {excerpt: {ne: ""}}}) {
 		nodes {
 			childMdx {
 				slug
@@ -35,8 +24,10 @@ query {
 				frontmatter {
 					title
 					description
+					order
 				}
 			}
+			relativeDirectory
 		}
 	}
 }
--- src/pages/guides.js
+++ src/pages/guides.js
@@ -2,75 +2,13 @@ import React from 'react'
 import { graphql, Link } from "gatsby"
 import Layout from '../layouts/layout'
 import { StaticImage } from 'gatsby-plugin-image'
-
-// https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects?page=1&tab=votes#tab-top
-const groupBy = function (xs, key) {
-	return xs.reduce(function (rv, x) {
-		let v = key instanceof Function ? key(x) : x[key];
-		let el = rv.find((r) => r && r.key === v);
-		if (el) {
-			el.values.push(x);
-		} else {
-			rv.push({ key: v, values: [x] });
-		}
-		return rv;
-	}, []);
-}
-
-// https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
-const alphaSort = function (a, b) {
-	let ca = a.key
-	let cb = b.key
-
-	if (ca < cb) return -1
-	if (ca > cb) return 1
-	return 0
-}
-
-const numSort = function (a, b) {
-	// https://usefulangle.com/post/281/javascript-default-value-if-null-or-undefined
-	let na = a.childMdx.frontmatter.order
-	let nb = b.childMdx.frontmatter.order
-
-	return na - nb
-}
+import SubpagesTable from '../components/SubpagesTable'
 
 export default function guides({ data }) {
 	return (
 		<Layout>
 			<StaticImage src="../images/guides.png" placeholder="blurred"/>
-			{
-				groupBy(data.allFile.nodes, "relativeDirectory").sort(alphaSort).map(group => {
-					return (
-						<div>
-							<h1>{group.key.replace('_', ' ')}</h1>
-							<table class="min-w-full divide-y divide-gray-200">
-								<thead>
-									<tr>
-										<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-										<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>
-									</tr>
-								</thead>
-								<tbody>
-									{
-										group.values.sort(numSort).map(node => {
-											var slug = node.childMdx.slug
-											var title = node.childMdx.frontmatter.title
-											var description = node.childMdx.frontmatter.description
-											return (
-												<tr key={slug}>
-													<td class="px-6 py-4 whitespace-nowrap"><Link to={slug}>{title}</Link></td>
-													<td class="px-6 py-4 whitespace-nowrap"><Link to={slug}>{description}</Link></td>
-												</tr>
-											)
-										})
-									}
-								</tbody>
-							</table>
-						</div>
-					)
-				})
-			}
+			<SubpagesTable nodes={data.allFile.nodes}/>
 			
 		</Layout>
 	)
