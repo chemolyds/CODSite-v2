fb75606c7d88ad7a7366e50b3d54e77dffd5f423
Jerdan1980 <jeremytinana@gmail.com>
2021-07-16 15:10:44 -0400

    Implement tables in silver and gold

--- src/pages/courses/Gold.js
+++ src/pages/courses/Gold.js
@@ -1,25 +1,14 @@
 import React from 'react'
 import { graphql, Link } from "gatsby"
 import Layout from '../../layouts/layout'
+import SubpagesTable from '../../components/SubpagesTable'
 
 export default function Gold({ data }) {
 	return (
 		<Layout>
 			<h1>Gold</h1>
 			<Link to='/courses'>Back to Courses</Link>
-			<ul>
-			{
-				data.allFile.nodes.filter(node => node.childMdx).filter(node => node.childMdx.excerpt).map(node => {
-					var slug = node.childMdx.slug
-					var title = node.childMdx.frontmatter.title
-					return (
-						<li><Link to={slug}>
-							{title ? title : slug}
-						</Link></li>
-					)
-				})
-			}
-			</ul>
+			<SubpagesTable nodes={data.allFile.nodes}/>
 			
 		</Layout>
 	)
@@ -27,7 +16,7 @@ export default function Gold({ data }) {
 
 export const query = graphql`
 query {
-	allFile(filter: {sourceInstanceName: {eq: "Gold"}}) {
+	allFile(filter: {sourceInstanceName: {eq: "Gold"}, childMdx: {excerpt: {ne: ""}}}) {
 		nodes {
 			childMdx {
 				slug
@@ -35,8 +24,10 @@ query {
 				frontmatter {
 					title
 					description
+					order
 				}
 			}
+			relativeDirectory
 		}
 	}
 }
