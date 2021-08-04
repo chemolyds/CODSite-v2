34665715fba143897e0b56764e233ef34fbf3c16
Jerdan1980 <jeremytinana@gmail.com>
2021-08-04 14:20:37 -0400

    Resources update

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -4,6 +4,33 @@ import Layout from '../layouts/layout'
 import stuff from './resources.json'
 import autoprefixer from 'autoprefixer'
 
+const GeneralResources = [
+	{
+		"title": "CODS Courses (WIP)",
+		"description": "We provide over 60 courses for all participants to use, from topics that are regularly covered in your classroom to Olympiad-specific topics. These courses are designed specifically for the chemistry Olympiad competitors and those who plan on competing in CODS Competitions.\nEach module provides resources, special guidance, and problems for participants to use for thier learning experience. We updte the courses every week and anyone can contribute to the project. Check it out!",
+		"image": "https://imgur.com/1NOksZT.png"
+	},
+	{
+		"title": "Chemguide",
+		"description": "Chemguide is a very good learning resource. Made by Jim Clark, its initial purpose was to provide U.K. students a place to learn about chemistry. However, this resource is extremely helpful for chemistry Olympiad participants as well, and the inorganic chemistry section in particular has been proven to be invaluable.",
+		"image": "https://imgur.com/upd9Nwt.png",
+		"link": "https://www.chemguide.co.uk/"
+	},
+	{
+		"title": "Chemistry LibreTexts",
+		"description": "Chemistry LibreTexts can also be great help as well. Content that is present in Chemistry Olympiads that aren't taught in high school can often be learned from this website. You should check it out!",
+		"image": "https://imgur.com/F2HO00G.png",
+		"link": "https://chem.libretexts.org/"
+	},
+	{
+		"title": "CODSNotes",
+		"description": "Notes written by Fizzest",
+		"image": "https://imgur.com/1NOksZT.png",
+		"link": "https://drive.google.com/drive/folders/195kcNDwljvw1w5ZKVWzpE7LCdbvwQzVL?usp=sharing",
+		"download": "https://drive.google.com/drive/folders/195kcNDwljvw1w5ZKVWzpE7LCdbvwQzVL?usp=sharing"
+	}
+]
+
 const numcols = function (num) {
 	let three = num % 3
 	let four = num % 4
@@ -21,6 +48,28 @@ export default function resources() {
 	return (
 		<Layout>
 			<h1>Resources</h1>
+
+			<div class="bg-gray-200 pt-5 pb-1 mt-10 mb-5">
+				<h2 class="text-center">General Resources</h2>
+			</div>
+			{GeneralResources.map(r => {
+				return (
+					<div class="md:grid md:grid-cols-3 gap-5">
+						<div>
+							{r.image ? <img src={r.image}/> : <></>}
+						</div>
+						<div class="col-span-2 mb-10">
+							{r.title ? <p class="font-bold mt-0 mb-1">
+								{r.link ? <Link to={r.link}>{r.title}</Link> : r.title}
+							</p> : <></>}
+							<p class="text-sm mb-1">
+								{r.link ? <Link to={r.link}>{r.description}</Link> : r.description}
+							</p>
+							{r.download? <Link class="text-blue-400 font-bold mt-0" to={r.download}>Download</Link> : <></>}
+						</div>
+					</div>
+				)
+			})}
 
 			{stuff.map(c => {
 				return (
