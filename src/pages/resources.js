91f27627c51b1354e878c99dce2d27cad1be02a4
Jerdan1980 <jeremytinana@gmail.com>
2021-08-04 17:09:36 -0400

    Mess with spacing

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Layout from '../layouts/layout'
+
+import SEO from '../components/seo'
+import Base from '../layouts/base'
 import stuff from './resources.json'
-import autoprefixer from 'autoprefixer'
 
 const GeneralResources = [
 	{
 		"title": "CODS Courses (WIP)",
-		"description": "We provide over 60 courses for all participants to use, from topics that are regularly covered in your classroom to Olympiad-specific topics. These courses are designed specifically for the chemistry Olympiad competitors and those who plan on competing in CODS Competitions.\nEach module provides resources, special guidance, and problems for participants to use for thier learning experience. We updte the courses every week and anyone can contribute to the project. Check it out!",
+		"description": "We provide over 60 courses for all participants to use, from topics that are regularly covered in your classroom to Olympiad-specific topics. These courses are designed specifically for the chemistry Olympiad competitors and those who plan on competing in CODS Competitions.\n\nEach module provides resources, special guidance, and problems for participants to use for thier learning experience. We updte the courses every week and anyone can contribute to the project. Check it out!",
 		"image": "https://imgur.com/1NOksZT.png"
 	},
 	{
@@ -26,8 +27,7 @@ const GeneralResources = [
 		"title": "CODSNotes",
 		"description": "Detailed notes on competitive chemistry topics, based on Atkins Chemical Principles",
 		"image": "https://imgur.com/1NOksZT.png",
-		"link": "https://drive.google.com/drive/folders/195kcNDwljvw1w5ZKVWzpE7LCdbvwQzVL?usp=sharing",
-		"download": "https://drive.google.com/drive/folders/195kcNDwljvw1w5ZKVWzpE7LCdbvwQzVL?usp=sharing"
+		"link": "https://drive.google.com/drive/folders/195kcNDwljvw1w5ZKVWzpE7LCdbvwQzVL?usp=sharing"
 	}
 ]
 
@@ -46,25 +46,23 @@ const numcols = function (num) {
 
 export default function resources() {
 	return (
-		<Layout>
-			<h1>Resources</h1>
+		<Base>
+			<SEO title="Resources"/>
+			<h1 class="text-center">Resources</h1>
 
 			<div class="bg-gray-200 pt-5 pb-1 mt-10 mb-5">
 				<h2 class="text-center">General Resources</h2>
 			</div>
 			{GeneralResources.map(r => {
 				return (
-					<div class="md:grid md:grid-cols-3 gap-5">
+					<div class="md:grid md:grid-cols-3 gap-5 px-5 md:px-20">
 						<div>
 							{r.image ? <img src={r.image}/> : <></>}
 						</div>
 						<div class="col-span-2 mb-10">
-							{r.title ? <p class="font-bold mt-0 mb-1">
-								{r.link ? <Link to={r.link}>{r.title}</Link> : r.title}
-							</p> : <></>}
-							<p class="text-sm mb-1">
-								{r.link ? <Link to={r.link}>{r.description}</Link> : r.description}
-							</p>
+							{r.title ? <h1 class="font-bold mt-0 mb-1">{r.title}</h1> : <></>}
+							<p class="text-sm mt-2 mb-1">{r.description}</p>
+							{r.link ? <Link class="text-blue-400 font-bold mt-0" to={r.link}>Link</Link> : <></>}
 							{r.download? <Link class="text-blue-400 font-bold mt-0" to={r.download}>Download</Link> : <></>}
 						</div>
 					</div>
@@ -78,17 +76,14 @@ export default function resources() {
 							<h2 class="text-center">{c.category}</h2>
 							{c.description ? <p class="text-center">{c.description}</p> : <></>}
 						</div>
-						<div class={`md:grid gap-5 place-items-center ${numcols(c.resources.length)}`}>
+						<div class={`md:grid gap-5 place-items-center ${numcols(c.resources.length)} px-5 md:px-20`}>
 							{c.resources.map(r => {
 									return (
 										<div class="text-center mb-10">
 											{r.image ? <img src={r.image} class="h-64 mb-1"/> : <></>}
-											{r.title ? <p class="font-bold mt-0 mb-1">
-												{r.link ? <Link to={r.link}>{r.title}</Link> : r.title}
-											</p> : <></>}
-											<p class="text-sm mb-1">
-												{r.link ? <Link to={r.link}>{r.description}</Link> : r.description}
-											</p>
+											{r.title ? <p class="font-bold mt-0 mb-1">{r.title}</p> : <></>}
+											<p class="text-sm mb-1">{r.description}</p>
+											{r.link ? <Link class="text-blue-400 font-bold mt-0" to={r.link}>Link</Link> : <></>}
 											{r.download? <Link class="text-blue-400 font-bold mt-0" to={r.download}>Download</Link> : <></>}
 										</div>
 									)
@@ -97,6 +92,6 @@ export default function resources() {
 					</div>
 				)
 			})}
-		</Layout>
+		</Base>
 	)
 }
